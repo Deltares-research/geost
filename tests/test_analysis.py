c3c34276c2522d3037079665559f7ba3295aa4b9
Erik van Onselen <erik.vanonselen@deltares.nl>
2022-11-22 15:43:53 +0100

    Test for pygef read, assert uses np.testing funcs

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+from numpy.testing import assert_equal, assert_allclose, assert_array_equal
 
 # Local imports
 from pysst.analysis.interpret_cpt import calc_ic, calc_lithology
@@ -33,26 +34,26 @@ class TestAnalysis:
     ):
         # After the first encounter of sand (Z) > 40% of the next 1 m must consist of sand as well
         # for this first encounter to be regarded as the top of the sand (and all the above the cover layer)
-        assert (
+        assert_equal(
             find_top_sand(
                 test_borehole_lith, test_borehole_top, test_borehole_bottom, 0.4, 1
-            )
-            == -3.0
+            ),
+            -3.0,
         )
 
         # After the first encounter of sand (Z) > 60% of the next 1 m must consist of sand as well
         # for this first encounter to be regarded as the top of the sand (and all the above the cover layer)
-        assert (
+        assert_equal(
             find_top_sand(
                 test_borehole_lith, test_borehole_top, test_borehole_bottom, 0.6, 1
-            )
-            == -4.0
+            ),
+            -4.0,
         )
 
     @pytest.mark.unittest
     def test_calc_ic(self, test_ic_array, test_fr_array):
         ic = calc_ic(test_ic_array, test_fr_array)
-        assert np.allclose(
+        assert_allclose(
             ic,
             np.array(
                 [
@@ -76,6 +77,6 @@ class TestAnalysis:
         lith = calc_lithology(
             calc_ic(test_ic_array, test_fr_array), test_ic_array, test_fr_array
         )
-        assert np.array_equal(
+        assert_array_equal(
             lith, np.array(["V", "Kh", "Kh", "K", "K", "K", "K", "Kz", "Z", "Z", "Z"])
         )
