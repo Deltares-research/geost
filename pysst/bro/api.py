75a7ec8d4f48b9f48bfff53c17c585d80b430f4a
Erik van Onselen <erik.vanonselen@deltares.nl>
2022-11-23 11:24:56 +0100

    Added exceptions to response

--- pysst/bro/api.py
+++ pysst/bro/api.py
@@ -18,4 +18,13 @@ class BroApi:
             response = self.session.get(
                 self.server_url + self.cpt_api + self.objects_url + f"/{bro_id}"
             )
-            yield etree.fromstring(response.text.encode("utf-8"))
+            if response.status_code == 200 and not "rejection" in response.text:
+                yield etree.fromstring(response.text.encode("utf-8"))
+            elif "rejection" in response.text:
+                raise Warning(
+                    f"{bro_id} is invalid and could not be retrieved from the database"
+                )
+            elif response.status_code != 200:
+                raise Warning(
+                    f"Error {response.status_code}: Unable to request {bro_id} from database"
+                )
--- tests/test_broapi.py
+++ tests/test_broapi.py
@@ -10,10 +10,27 @@ class TestBroApi:
         api = BroApi()
         assert api.session.get(api.server_url + api.cpt_api).status_code == 200
 
-    def test_get_cpts(self):
+    def test_get_single_valid_cpt(self):
+        api = BroApi()
+        cpts = api.get_cpt_objects("CPT000000038771")
+        for cpt in cpts:
+            assert isinstance(cpt, _Element)
+
+    def test_get_valid_cpts(self):
         api = BroApi()
         cpts = api.get_cpt_objects(
             ["CPT000000038771", "CPT000000000787", "CPT000000125133"]
         )
         for cpt in cpts:
             assert isinstance(cpt, _Element)
+
+    def test_get_invalid_cpt(self):
+        api = BroApi()
+        cpts = api.get_cpt_objects("CPT0000000doesnotexist")
+        with pytest.raises(Warning) as excinfo:
+            for cpt in cpts:
+                pass
+        assert (
+            "CPT0000000doesnotexist is invalid and could not be retrieved from the database"
+            in str(excinfo.value)
+        )
