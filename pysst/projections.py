13983123ad9e28cb10dbdc6f2a635ea034c0ccb7
Erik van Onselen <erik.vanonselen@deltares.nl>
2022-12-12 10:25:34 +0100

    Fixed wrong coordinate tranformation

--- pysst/projections.py
+++ pysst/projections.py
@@ -3,10 +3,11 @@ import pyproj
 # TODO check conversion accuracy. Ellipsoid or bro problem
 
 
-def get_projection(epsg: str = "28992"):
-    return pyproj.Proj(init="epsg:" + epsg)
+def get_transformer(epsg_from, epsg_to):
+    return pyproj.Transformer.from_crs(epsg_from, epsg_to, accuracy=1.0)
 
 
 def xy_to_ll(x, y, epsg):
-    P = get_projection(epsg)
-    return P(x, y, inverse=True)
+    T = get_transformer(epsg, 4326)
+    transformed = list(T.itransform([(x, y)]))
+    return transformed[0][1], transformed[0][0]
--- tests/test_projections.py
+++ tests/test_projections.py
@@ -1,20 +1,18 @@
 import pytest
 from pathlib import Path
 from pysst import projections
+import pyproj
 from numpy.testing import assert_almost_equal
 
 
 class TestProjections:
     @pytest.mark.unittest
-    def test_get_projection(self):
-        P = projections.get_projection("28992")
-        assert (
-            P.definition
-            == "proj=sterea lat_0=52.1561605555556 lon_0=5.38763888888889 k=0.9999079 x_0=155000 y_0=463000 ellps=bessel units=m no_defs"
-        )
+    def test_get_transformer(self):
+        T = projections.get_transformer(28992, 4326)
+        assert type(T) == pyproj.transformer.Transformer
 
     @pytest.mark.unittest
     def test_xy_to_ll(self):
-        lon_dec, lat_dec = projections.xy_to_ll(141000, 455000, "28992")
-        assert_almost_equal(lon_dec, 5.1833624)
-        assert_almost_equal(lat_dec, 52.0840712)
+        lon_dec, lat_dec = projections.xy_to_ll(141000, 455000, 28992)
+        assert_almost_equal(lon_dec, 5.182956740521864)
+        assert_almost_equal(lat_dec, 52.083091710433095)
