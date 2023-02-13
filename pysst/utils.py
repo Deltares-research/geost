2ceb540165377659b0c79207ebc9bbc2be9e1015
Bas Knaake <bas.knaake@gmail.com>
2023-02-13 17:05:46 +0100

    Added utils.safe_float and added work gef parser

--- pysst/utils.py
+++ pysst/utils.py
@@ -4,6 +4,18 @@ from typing import Union
 import pandas as pd
 
 
+class MissingOptionalModule:
+    """
+    Presents a clear error for optional modules.
+    
+    """
+    def __init__(self, name):
+        self.name = name
+
+    def __getattr__(self, name):
+        raise ImportError(f"{self.name} is required for this functionality")
+        
+
 def csv_to_parquet(
     file: Union[str, WindowsPath], out_file: Union[str, WindowsPath] = None, **kwargs
 ) -> None:
@@ -79,13 +91,8 @@ def get_path_iterable(path: WindowsPath, wildcard: str = "*"):
         raise TypeError("Given path is not a file or a folder")
 
 
-class MissingOptionalModule:
-    """
-    Presents a clear error for optional modules.
-    """
-
-    def __init__(self, name):
-        self.name = name
-
-    def __getattr__(self, name):
-        raise ImportError(f"{self.name} is required for this functionality")
+def safe_float(number):
+    try:
+        return float(number)
+    except ValueError:
+        return None
