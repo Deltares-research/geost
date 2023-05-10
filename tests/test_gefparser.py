bf5c118573fc54f23618d417729fc242436afc3b
Bas Knaake <bas.knaake@gmail.com>
2023-05-10 15:32:15 +0200

    Updated fixtures and integration test

--- tests/test_gefparser.py
+++ tests/test_gefparser.py
@@ -30,63 +30,45 @@ class TestCptGefParser:
         cpt = CptGefFile(
             self.filepath/r'83268_DKMP003_(DKMP_D03)_wiertsema.gef'
         )
-        return cpt
+        info_to_test = CptInfo(
+            'DKMP_D03', 176161.1, 557162.1, -0.06, 37.317295, 11, '31000', 'NAP', 0.01, 1815
+            )
+        return cpt, info_to_test
 
     @pytest.fixture
     def cpt_b(self):
         cpt = CptGefFile(self.filepath/r'AZZ158_gem_rotterdam.gef')
-        return cpt
+        info_to_test = CptInfo(
+            'AZZ158', 0.0, 0.0, 5.05, 59.5, 6, '0', 'NAP', None, 2980
+            )
+        return cpt, info_to_test
 
     @pytest.fixture
     def cpt_c(self):
         cpt = CptGefFile(self.filepath/r'CPT000000157983_IMBRO.gef')
-        return cpt
+        info_to_test = CptInfo(
+            'CPT000000157983', 176416.1, 557021.9, -5.5, 28.84, 9, '28992', 'NAP', None, 1310
+            )
+        return cpt, info_to_test
 
     @pytest.fixture
     def cpt_d(self):
         cpt = CptGefFile(self.filepath/r'CPT10_marine_sampling.gef')
-        return cpt
-    
-    @pytest.fixture
-    def test_info_cpt_a(self):
-        return CptInfo(
-            'DKMP_D03', 176161.1, 557162.1, -0.06, 37.317295, 11, '31000', 'NAP', 0.01, 1815
-            )
-    
-    @pytest.fixture
-    def test_info_cpt_b(self):
-        return CptInfo(
-            'AZZ158', 0.0, 0.0, 5.05, 59.5, 6, '0', 'NAP', None, 2980
-            )
-
-    @pytest.fixture
-    def test_info_cpt_c(self):
-        return CptInfo(
-            'CPT000000157983', 176416.1, 557021.9, -5.5, 28.84, 9, '28992', 'NAP', None, 1310
-            )
-    
-    @pytest.fixture
-    def test_info_cpt_d(self):
-        return CptInfo(
+        info_to_test = CptInfo(
             'YANGTZEHAVEN CPT 10', 61949.0, 443624.0, -17.69, 5.75, 4, '31000', 'NAP', None, None
             )
+        return cpt, info_to_test
 
     @pytest.mark.unittest
     def test_read_files(self, test_cpt_files):
         for f in test_cpt_files:
             cpt = CptGefFile(f)
             assert isinstance(cpt, CptGefFile)
-    
+
     @pytest.mark.integrationtest
-    @pytest.mark.parametrize('cpt, test_info', [
-        ('cpt_a', 'test_info_cpt_a'),
-        ('cpt_b', 'test_info_cpt_b'),
-        ('cpt_c', 'test_info_cpt_c'),
-        ('cpt_d', 'test_info_cpt_d'),
-    ])
-    def test_cpt_parsing_result(self, cpt, test_info, request):
-        cpt = request.getfixturevalue(cpt)
-        test_info = request.getfixturevalue(test_info)
+    @pytest.mark.parametrize('cpt_test', ['cpt_a', 'cpt_b', 'cpt_c', 'cpt_d'])
+    def test_cpt_parsing_result(self, cpt_test, request):
+        cpt, test_info = request.getfixturevalue(cpt_test)
 
         assert cpt.nr == test_info.nr
         assert cpt.x == test_info.x
@@ -103,4 +85,3 @@ class TestCptGefParser:
 
         if cpt.nr != 'YANGTZEHAVEN CPT 10':  # TODO: Fix Yangtzehaven parsing bug.
             assert len(cpt.df) == test_info.nrecords
-  
