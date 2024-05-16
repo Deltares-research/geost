1e2e614fc3a74afabc598eef425ddb113f63eb21
Erik van Onselen <erik.vanonselen@deltares.nl>
2024-05-16 10:14:17 +0200

    Updated spatial utils tests

---
+++ tests/test_spatial_utils.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+import geopandas as gpd
+import pandas as pd
+import pytest
+from numpy.testing import assert_allclose
+
+from geost import read_sst_cores
+from geost.borehole import BoreholeCollection
+
+borehole_file = Path(__file__).parent / "data" / "test_boreholes.parquet"
+selection_file = Path(__file__).parent / "data" / "test_polygon.parquet"
+
+
+class TestPointCollection:
+
+    @pytest.fixture
+    def boreholes(self):
+        borehole_collection = read_sst_cores(self.borehole_file)
+        return borehole_collection
+
+    @pytest.mark.unittest
+    def test_get_raster_values():
+        pass
