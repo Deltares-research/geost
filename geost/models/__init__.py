7cd538ace7a131bcb32d1c8bf0696df40c029f4d
smknaake <bas.knaake@deltares.nl>
2024-09-26 15:57:50 +0200

    Implement utility functions for support of BRO distributed models

---
+++ geost/models/__init__.py
@@ -0,0 +1,1 @@
+from .basemodels import LayerModel, VoxelModel
---
+++ tests/bro/test_bro_utils.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pytest
+import xarray as xr
+from numpy.testing import assert_array_equal
+
+from geost.bro.bro_utils import coordinates_to_cellcenters, flip_ycoordinates
+
+
+@pytest.fixture
+def dataset():
+    coords = np.arange(3)
+    return xr.DataArray(
+        np.ones((3, 3, 3)),
+        coords={"y": coords, "x": coords, "z": [0.5, 1.5, 2.5]},
+        dims=("y", "x", "z"),
+    )
+
+
+@pytest.mark.unittest
+def test_coordinates_to_cellcenters(dataset):
+    cellsize, dz = 1, 1
+    dataset = coordinates_to_cellcenters(dataset, cellsize, dz)
+    assert_array_equal(dataset["x"], [0.5, 1.5, 2.5])
+    assert_array_equal(dataset["y"], [0.5, 1.5, 2.5])
+    assert_array_equal(dataset["z"], [1, 2, 3])
+
+
+@pytest.mark.unittest
+def test_flip_ycoordinates(dataset):
+    dataset = flip_ycoordinates(dataset)
+    assert_array_equal(dataset["y"], [2, 1, 0])
+    assert dataset.dims == ("y", "x", "z")
