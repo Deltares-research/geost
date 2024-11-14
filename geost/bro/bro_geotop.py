7ef42677add9fe8e908359d562495412b14b7e75
smknaake <bas.knaake@deltares.nl>
2024-11-14 14:34:38 +0100

    Add `UnitEnum` for grouping of Geotop units; separate bro_geotop and bro_regis

--- geost/bro/bro_geotop.py
+++ geost/bro/bro_geotop.py
@@ -4,9 +4,134 @@ from typing import List
 import rioxarray as rio
 import xarray as xr
 
-from geost.models import LayerModel, VoxelModel
+from geost.enums import UnitEnum
+from geost.models import VoxelModel
 
 from .bro_utils import coordinates_to_cellcenters, flip_ycoordinates
+
+
+class Lithology(UnitEnum):
+    anthropogenic = 0
+    organic = 1
+    clay = 2
+    loam = 3
+    fine_sand = 5
+    medium_sand = 6
+    coarse_sand = 7
+    gravel = 8
+    shells = 9
+
+
+class HoloceneUnits(UnitEnum):
+    NIGR = 1010
+    NINB = 1045
+    NASC = 1020
+    ONAWA = 1030
+    NAZA = 1040
+    NAWAZU = 1048
+    NAWAAL = 1049
+    NAWA = 1050
+    BHEC = 1060
+    OEC = 1070
+    NAWOBE = 1080
+    KK1 = 1085
+    NIFL = 1089
+    NIHO = 1090
+    NAZA2 = 1095
+    NAWO = 1100
+    NWNZ = 1110
+    NAWOVE = 1120
+    KK2 = 1125
+    NIBA = 1130
+    NA = 2000
+    EC = 2010
+    NI = 2020
+    KK = 2030
+
+
+class OlderUnits(UnitEnum):
+    BXKO = 3000
+    BXSI = 3010
+    BXSI1 = 3011
+    BXWI = 3020
+    BXSI2 = 3012
+    BXWIKO = 3025
+    BXWISIKO = 3030
+    BXDE = 3040
+    BXDEKO = 3045
+    BXSC = 3050
+    BXLM = 3060
+    BXBS = 3090
+    BX = 3100
+    KRWY = 4000
+    KRBXDE = 4010
+    KRZU = 4020
+    KROE = 4030
+    KRTW = 4040
+    KR = 4050
+    BEOM = 4055
+    BEWY = 4060
+    BERO = 4070
+    BE = 4080
+    KW1 = 4085
+    KW = 4090
+    WB = 4100
+    EE = 4110
+    EEWB = 4120
+    DR = 5000
+    DRGI = 5010
+    GE = 5020
+    DN = 5030
+    URTY = 5040
+    PE = 5050
+    UR = 5060
+    ST = 5070
+    AP = 5080
+    SY = 5090
+    PZ = 5100
+    WA = 5110
+    PZWA = 5120
+    MS = 5130
+    KI = 5140
+    OO = 5150
+    IE = 5160
+    VI = 5170
+    BR = 5180
+    VE = 5185
+    RUBO = 5190
+    RU = 5200
+    TOZEWA = 5210
+    TOGO = 5220
+    TO = 5230
+    DOAS = 5240
+    DOIE = 5250
+    DO = 5260
+    LA = 5270
+    HT = 5280
+    HO = 5290
+    MT = 5300
+    GU = 5310
+    VA = 5320
+    AK = 5330
+
+
+class ChannelBeltUnits(UnitEnum):
+    AEC = 6000
+    ABEOM = 6005
+    ANAWA = 6010
+    ANAWO = 6020
+    BEC = 6100
+    BNAWA = 6110
+    BNAWO = 6120
+    CEC = 6200
+    CNAWA = 6210
+    CNAWO = 6220
+    DEC = 6300
+    DNAWA = 6310
+    DNAWO = 6320
+    EEC = 6400
+    ENAWA = 6410
+    ENAWO = 6420
 
 
 class GeoTop(VoxelModel):
@@ -107,7 +232,3 @@ class GeoTop(VoxelModel):
 
         """
         return cls.from_netcdf(url, data_vars, bbox, lazy, **xr_kwargs)
-
-
-class Regis(LayerModel):
-    pass
--- geost/enums.py
+++ geost/enums.py
@@ -1,4 +1,57 @@
-from enum import StrEnum
+from enum import IntEnum, StrEnum
+from typing import Iterable
+
+
+class UnitEnum(IntEnum):
+    """
+    Extension of IntEnum to be used for classified values of stratigraphic units and
+    lithologies in models to allow easy selections.
+
+    """
+
+    @classmethod
+    def select_units(cls, units: str | Iterable[str]):
+        """
+        Select units by name.
+
+        Parameters
+        ----------
+        units : str | Iterable[str]
+            Name as string or array_like object of strings with names to select.
+
+        Returns
+        -------
+        List[enum]
+            List of the selected units.
+
+        """
+        units = cls.cast_to_list(units)
+        return [u for u in cls if u.name in units]
+
+    @classmethod
+    def select_values(cls, values: int | Iterable[int]):
+        """
+        Select units by value.
+
+        Parameters
+        ----------
+        units : str | Iterable[str]
+            Name as string or array_like object of strings with names to select.
+
+        Returns
+        -------
+        List[enum]
+            List of the selected units.
+
+        """
+        values = cls.cast_to_list(values)
+        return [n for n in cls if n.value in values]
+
+    @staticmethod
+    def cast_to_list(value):
+        if isinstance(value, str):
+            value = [value]
+        return value
 
 
 class VerticalReference(StrEnum):
