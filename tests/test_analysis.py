7c330038cca359b752f77c8cedbf85ba85f6c66c
smknaake <bas.knaake@deltares.nl>
2024-12-13 10:39:31 +0100

    Fix depth problem with find_top_sand

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -26,11 +26,11 @@ class TestAnalysis:
 
     @pytest.fixture
     def test_borehole_top(self):
-        return np.array([0, -1, -1.5, -3, -3.5, -4, -6])
+        return np.array([0, 1, 1.5, 3, 3.5, 4, 6])
 
     @pytest.fixture
     def test_borehole_bottom(self):
-        return np.array([-1, -1.5, -3, -3.5, -4, -6, -10])
+        return np.array([1, 1.5, 3, 3.5, 4, 6, 10])
 
     @pytest.fixture
     def test_ic_array(self):
@@ -47,22 +47,33 @@ class TestAnalysis:
         # After the first encounter of sand (Z) > 40% of the next 1 m must consist of
         # sand as well for this first encounter to be regarded as the top of the sand
         # (and all the above the cover layer)
-        assert_equal(
-            find_top_sand(
+        top = find_top_sand(
                 test_borehole_lith, test_borehole_top, test_borehole_bottom, 0.4, 1
-            ),
-            -3.0,
-        )
+            )
+        assert_equal(top, 3.0)
 
         # After the first encounter of sand (Z) > 60% of the next 1 m must consist of
         # sand as well for this first encounter to be regarded as the top of the sand
         # (and all the above the cover layer)
-        assert_equal(
-            find_top_sand(
+        top = find_top_sand(
                 test_borehole_lith, test_borehole_top, test_borehole_bottom, 0.6, 1
-            ),
-            -4.0,
-        )
+            )
+        assert_equal(top, 4.0)
+
+        top = find_top_sand(
+                test_borehole_lith, test_borehole_top, test_borehole_bottom, 0.5, 1
+            )
+        assert_equal(top, 3.0)
+
+        top = find_top_sand(
+                test_borehole_lith, test_borehole_top, test_borehole_bottom, 1, 6.5
+            )
+        assert_equal(top, np.nan)
+
+        top = find_top_sand(
+                test_borehole_lith, test_borehole_top, test_borehole_bottom, 0.91, 6.5
+            )
+        assert_equal(top, 3.0)
 
     @pytest.mark.unittest
     def test_calc_ic(self, test_ic_array, test_fr_array):
