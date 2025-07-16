422000c1a3ce6c7815da757aef97d7f47b76817f
smknaake <bas.knaake@deltares.nl>
2025-07-16 14:28:32 +0200

    Layout for xml parsing using defined schemas for xml files

---
+++ geost/io/xml/resolver.py
@@ -0,0 +1,49 @@
+from typing import Any
+
+from lxml import etree
+
+
+def parse_crs(el: etree.Element, **_) -> str:
+    """
+    Retrieve the coordinate reference system (CRS) from an XML element.
+
+    Parameters
+    ----------
+    el : etree.Element
+        The XML element containing the CRS information.
+
+    Returns
+    -------
+    str
+        String representing the CRS (e.g. 'urn:ogc:def:crs:EPSG::28992'), or "unknown"
+        if not found.
+
+    """
+    return el.attrib.get("srsName", "unknown")
+
+
+def parse_coordinates(coords: str, **_) -> tuple[float, float]:
+    """
+    Parse a string containing coordinates as a tuple of floats.
+
+    Parameters
+    ----------
+    coords : str
+        Any of {'x y', 'x,y', 'x;y'} where x and y are parsable by float
+
+    Returns
+    -------
+    tuple[float, float]
+        The parsed coordinates as a tuple of floats.
+
+    """
+    if " " in coords:
+        splitter = " "
+    elif "," in coords:
+        splitter = ","
+    elif ";" in coords:
+        splitter = ";"
+    else:
+        raise ValueError(f"pygef does not know how to parse '{coords}' coordsition")
+    parts = coords.split(splitter)
+    return float(parts[0]), float(parts[1])
