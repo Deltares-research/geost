f24c1b84f4b2586ded2b05a25fb821563396066b
smknaake <bas.knaake@deltares.nl>
2026-02-12 16:23:27 +0100

    First commit single-accessor; copy easy most easy to implement methods

---
+++ geost/accessor.py
@@ -0,0 +1,482 @@
+from typing import TYPE_CHECKING, Any, Iterable, List
+
+import geopandas as gpd
+import pandas as pd
+from pyproj import CRS
+from shapely import buffer
+
+from geost import spatial
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    from shapely.geometry.base import BaseGeometry
+
+type DataFrame = pd.DataFrame | gpd.GeoDataFrame
+type GeometryType = BaseGeometry | list[BaseGeometry]
+
+
+@pd.api.extensions.register_dataframe_accessor("gst")
+class GeostFrame:
+    def __init__(self, dataframe: DataFrame):
+        self._validate_dataframe(dataframe)
+        self._obj = dataframe
+
+    @staticmethod
+    def _validate_dataframe(dataframe):
+        if "nr" not in dataframe.columns:
+            raise KeyError(
+                "DataFrame must contain an 'nr' column identifying individual objects."
+            )
+
+    @property
+    def has_geometry(self):
+        """
+        Returns True if the object is a GeoDataFrame with a valid geometry column, False
+        otherwise. Used to determine whether specfic geospatial operations can be performed
+        on the object.
+
+        """
+        if isinstance(self._obj, gpd.GeoDataFrame):
+            if self._obj._geometry_column_name is not None:
+                # Only if self._obj is a GeoDataFrame we can check this attribute
+                return True
+        return False
+
+    @property
+    def has_depth_columns(self):
+        """
+        Returns True if the object contains information about depth, such as 'top' and
+        'bottom' or 'depth' columns, False otherwise. Used to determine whether specific
+        selection operations can be performed on the object.
+
+        """
+        # Check if the dataframe contains columns that indicate depth information.
+
+        # This can be:
+        # - 'top' and 'bottom' columns (layered data)
+        # - 'depth' column (discrete data)
+        # - Only 'bottom' column (also discrete data)
+
+        # We also need to check the presence of a 'surface' column
+
+        if "surface" not in self._obj.columns:
+            return False
+
+        return "bottom" in self._obj.columns  # will be changed to 'depth' in the future
+
+    @staticmethod
+    def _to_iterable(selection_values: str | Iterable) -> Iterable:
+        if isinstance(selection_values, str):
+            selection_values = [selection_values]
+        return selection_values
+
+    def _check_has_spatial(self):
+        if not self.has_geometry:
+            raise TypeError(
+                "Object is not a GeoDataFrame with a valid geometry column."
+            )
+
+    def _check_has_depth(self):
+        if not self.has_depth_columns:
+            raise TypeError(
+                "Object does not contain depth information (e.g., 'top' and 'bottom' or "
+                "'depth' columns)."
+            )
+
+    def change_horizontal_reference(self, to_epsg: str | int | CRS) -> None:
+        raise NotImplementedError("Method not implemented yet.")
+
+    def change_vertical_reference(
+        self, from_epsg: str | int | CRS, to_epsg: str | int | CRS
+    ) -> None:
+        raise NotImplementedError("Method not implemented yet.")
+
+    def select_within_bbox(
+        self,
+        xmin: int | float,
+        ymin: int | float,
+        xmax: int | float,
+        ymax: int | float,
+        invert: bool = False,
+    ) -> gpd.GeoDataFrame:
+        """
+        Select data within a specified bounding box (xmin, ymin, xmax, ymax).
+
+        Parameters
+        ----------
+        xmin : float | int
+            Minimum x-coordinate of the bounding box.
+        ymin : float | int
+            Minimum y-coordinate of the bounding box.
+        xmax : float | int
+            Maximum x-coordinate of the bounding box.
+        ymax : float | int
+            Maximum y-coordinate of the bounding box.
+        invert : bool, optional
+            Invert the selection, so select all objects outside of the
+            bounding box in this case, by default False.
+
+        Returns
+        -------
+        gpd.GeoDataFrame
+            GeoDataFrame instance containing only the selected geometries.
+
+        """
+        self._check_has_spatial()
+        selection = spatial.select_points_within_bbox(
+            self._obj, xmin, ymin, xmax, ymax, invert=invert
+        )
+        return selection
+
+    def select_with_points(
+        self,
+        points: str | Path | gpd.GeoDataFrame | GeometryType,
+        max_distance: float | int,
+        invert: bool = False,
+    ) -> gpd.GeoDataFrame:
+        """
+        Select data based on the distance to given point geometries.
+
+        Parameters
+        ----------
+        points : str | Path | gpd.GeoDataFrame | GeometryType
+            Any type of point geometries that can be used for the selection: GeoDataFrame
+            containing points or filepath to a shapefile like file, or Shapely Point,
+            MultiPoint or list containing Point objects.
+        max_distance : float | int
+            Maximum distance for selection geometries.
+        invert : bool, optional
+            Invert the selection, by default False.
+
+        Returns
+        -------
+        gpd.GeoDataFrame
+            GeoDataFrame instance containing only the selected geometries.
+
+        """
+        self._check_has_spatial()
+        selection = spatial.select_points_near_points(
+            self._obj, points, max_distance, invert=invert
+        )
+        return selection
+
+    def select_with_lines(
+        self,
+        lines: str | Path | gpd.GeoDataFrame | GeometryType,
+        max_distance: float | int,
+        invert: bool = False,
+    ) -> gpd.GeoDataFrame:
+        """
+        Select data based on the distance to given line geometries.
+
+        Parameters
+        ----------
+        lines : str | Path | gpd.GeoDataFrame | GeometryType
+            Any type of line geometries that can be used for the selection: GeoDataFrame
+            containing lines or filepath to a shapefile like file, or Shapely LineString,
+            MultiLineString or list containing LineString objects.
+        max_distance : float | int
+            Maximum distance for selection geometries.
+        invert : bool, optional
+            Invert the selection, by default False.
+
+        Returns
+        -------
+        gpd.GeoDataFrame
+            GeoDataFrame instance containing only the selected geometries.
+
+        """
+        self._check_has_spatial()
+        selection = spatial.select_points_near_lines(
+            self._obj, lines, max_distance, invert=invert
+        )
+        return selection
+
+    def select_within_polygons(
+        self,
+        polygons: str | Path | gpd.GeoDataFrame | GeometryType,
+        buffer: float | int = 0,
+        invert: bool = False,
+    ) -> gpd.GeoDataFrame:
+        """
+        Select data based on polygon geometries.
+
+        Parameters
+        ----------
+        polygons : str | Path | gpd.GeoDataFrame | GeometryType
+            Any type of polygon geometries that can be used for the selection: GeoDataFrame
+            containing polygons or filepath to a shapefile like file, or Shapely Polygon,
+            MultiPolygon or list containing Polygon objects.
+        buffer : float | int, optional
+            Optional buffer distance around the polygon selection geometries, by default
+            0.
+        invert : bool, optional
+            Invert the selection, by default False.
+
+        Returns
+        -------
+        gpd.GeoDataFrame
+            GeoDataFrame instance containing only the selected geometries.
+
+        """
+        self._check_has_spatial()
+        selection = spatial.select_points_within_polygons(
+            self._obj, polygons, buffer, invert=invert
+        )
+        return selection
+
+    def select_by_depth(
+        self,
+        top_min: float = None,
+        top_max: float = None,
+        end_min: float = None,
+        end_max: float = None,
+    ) -> gpd.GeoDataFrame:
+        raise NotImplementedError("Method not implemented yet.")
+
+    def get_area_labels(
+        self,
+        polygon_gdf: str | Path | gpd.GeoDataFrame,
+        column_name: str | Iterable,
+        include_in_header=False,
+    ) -> pd.DataFrame:
+        raise NotImplementedError("Method not implemented yet.")
+
+    def to_header(
+        self, horizontal_reference: str | int | CRS = 28992
+    ) -> gpd.GeoDataFrame:
+        raise NotImplementedError("Method not implemented yet.")
+
+    def to_collection(
+        self,
+        has_inclined: bool = False,
+        horizontal_reference: str | int | CRS = 28992,
+        vertical_reference: str | int | CRS = 5709,
+    ):
+        raise NotImplementedError("Method not implemented yet.")
+
+    def select_by_values(
+        self, column: str, selection_values: str | Iterable, how: str = "or"
+    ) -> pd.DataFrame:
+        """
+        Select data based on the presence of given values in a given column. Can be used
+        for example to select boreholes that contain peat in the lithology column.
+
+        Parameters
+        ----------
+        column : str
+            Name of column that contains categorical data to use when looking for
+            values.
+        selection_values : str | Iterable
+            Value or values to look for in the column.
+        how : str, optional
+            Either "and" or "or". "and" requires all selection values to be present in
+            column for selection. "or" will select the core if any one of the
+            selection_values are found in the column. Default is "and".
+
+        Returns
+        -------
+        pd.DataFrame
+            New DataFrame containing only the data selected by this method.
+
+        Examples
+        --------
+        To select boreholes where both clay ("K") and peat ("V") are present at the same
+        time, use "and" as a selection method:
+
+        >>> boreholes.select_by_values("lith", ["V", "K"], how="and")
+
+        To select boreholes that can have one, or both lithologies, use or as the selection
+        method:
+
+        >>> boreholes.select_by_values("lith", ["V", "K"], how="and")
+
+        """
+        if column not in self._obj.columns:
+            raise IndexError(
+                f"The column '{column}' does not exist and cannot be used for selection"
+            )
+
+        selection_values = self._to_iterable(selection_values)
+
+        selected = self._obj
+        if how == "or":
+            valid = self._obj["nr"][self._obj[column].isin(selection_values)].unique()
+            selected = selected[selected["nr"].isin(valid)]
+
+        elif how == "and":
+            for value in selection_values:
+                valid = self._obj["nr"][self._obj[column] == value].unique()
+                selected = selected[selected["nr"].isin(valid)]
+
+        return selected
+
+    def slice_depth_interval(
+        self,
+        upper_boundary: float | int = None,
+        lower_boundary: float | int = None,
+        relative_to_vertical_reference: bool = False,
+        update_layer_boundaries: bool = True,
+    ) -> pd.DataFrame:
+        self._check_has_depth()
+        raise NotImplementedError("Method not implemented yet.")
+
+    def slice_by_values(
+        self, column: str, selection_values: str | Iterable, invert: bool = False
+    ) -> pd.DataFrame:
+        """
+        Slice rows from data based on matching condition. E.g. only return rows with
+        a certain lithology in the collection object.
+
+        Parameters
+        ----------
+        column : str
+            Name of column that contains categorical data to use when looking for
+            values.
+        selection_values : str | Iterable
+            Values to look for in the column.
+        invert : bool, optional
+            If True, invert the slicing action, so remove layers with selected values
+            instead of keeping them. The default is False.
+
+        Returns
+        -------
+        pd.DataFrame
+            New DataFrame containing only the data objects selected by this method.
+
+        Examples
+        --------
+        Return only rows in data contain sand ("Z") as lithology:
+
+        >>> data.slice_by_values("lith", "Z")
+
+        If you want all the rows that may contain everything but sand, use the "invert"
+        option:
+
+        >>> data.slice_by_values("lith", "Z", invert=True)
+
+        """
+        selection_values = self._to_iterable(selection_values)
+
+        sliced = self._obj.copy()
+
+        if invert:
+            sliced = sliced[~sliced[column].isin(selection_values)]
+        else:
+            sliced = sliced[sliced[column].isin(selection_values)]
+
+        return sliced
+
+    def select_by_condition(self, condition: Any, invert: bool = False) -> pd.DataFrame:
+        """
+        Select data using a manual condition that results in a boolean mask. Returns the
+        rows in the data where the 'condition' evaluates to True.
+
+        Parameters
+        ----------
+        condition : list, pd.Series or array like
+            Boolean array like object with locations at which the values will be
+            preserved, dtype must be 'bool' and the length must correspond with the
+            length of the data.
+        invert : bool, optional
+            If True, the selection is inverted so rows that evaluate to False will be
+            returned. The default is False.
+
+        Returns
+        -------
+        pd.DataFrame
+            New DataFrame containing only the data selected by this method.
+
+        Examples
+        --------
+        Select rows in data that contain a specific value:
+
+        >>> data.select_by_condition(data["lith"]=="V")
+
+        Or select rows in the data that contain a specific (part of) string or strings:
+
+        >>> data.select_by_condition(data["column"].str.contains("foo|bar"))
+
+        """
+        if invert:
+            selected = self._obj[~condition]
+        else:
+            selected = self._obj[condition]
+
+        return selected
+
+    def get_cumulative_thickness(
+        self, column: str, values: str | List[str]
+    ) -> pd.DataFrame:
+        raise NotImplementedError("Method not implemented yet.")
+
+    def get_layer_top(
+        self,
+        column: str,
+        values: str | List[str],
+        min_thickness: float = 0,
+        min_depth: float = 0,
+    ) -> pd.DataFrame:
+        raise NotImplementedError("Method not implemented yet.")
+
+    def get_layer_base(
+        self,
+        column: str,
+        values: str | List[str],
+        min_thickness: float = 0,
+        min_depth: float = 0,
+    ) -> pd.DataFrame:
+        raise NotImplementedError("Method not implemented yet.")
+
+    def to_pyvista_cylinders(
+        self,
+        displayed_variables: str | List[str],
+        radius: float = 1,
+        vertical_factor: float = 1.0,
+        relative_to_vertical_reference: bool = True,
+    ):
+        raise NotImplementedError("Method not implemented yet.")
+
+    def to_pyvista_grid(
+        self,
+        displayed_variables: str | list[str],
+        radius: float = 1,
+    ):
+        raise NotImplementedError("Method not implemented yet.")
+
+    def to_datafusiontools(
+        self,
+        columns: List[str],
+        outfile: str | Path = None,
+        encode: bool = False,
+        relative_to_vertical_reference: bool = True,
+    ):
+        raise NotImplementedError("Method not implemented yet.")
+
+    def _create_geodataframe_3d(
+        self,
+        relative_to_vertical_reference: bool = True,
+        crs: str | int | CRS = None,
+        has_inclined: bool = False,
+    ):
+        raise NotImplementedError("Method not implemented yet.")
+
+    def to_qgis3d(
+        self,
+        outfile: str | Path,
+        relative_to_vertical_reference: bool = True,
+        crs: str | int | CRS = None,
+        has_inclined: bool = False,
+        **kwargs,
+    ):
+        raise NotImplementedError("Method not implemented yet.")
+
+    def to_kingdom(
+        self,
+        outfile: str | Path,
+        tdstart: int = 1,
+        vw: float = 1500.0,
+        vs: float = 1600.0,
+    ):
+        raise NotImplementedError("Method not implemented yet.")
--- tests/test_accessors.py
+++ tests/test_accessors.py
@@ -1,8 +1,229 @@
 import geopandas as gpd
 import pandas as pd
 import pytest
+import shapely
+from numpy.testing import assert_array_equal
+from shapely.geometry import LineString, Polygon
 
+from geost.accessor import GeostFrame
 from geost.accessors.accessor import DATA_BACKEND, HEADER_BACKEND
+
+
+@pytest.fixture
+def dataframe():
+    return pd.DataFrame({"nr": ["a", "b"], "x": [1, 2], "y": [3, 4]})
+
+
+@pytest.fixture
+def geodataframe(dataframe):
+    return gpd.GeoDataFrame(
+        dataframe, geometry=gpd.points_from_xy(dataframe.x, dataframe.y)
+    )
+
+
+class TestGeostFrame:
+    @pytest.mark.unittest
+    def test_accessor(self, dataframe: pd.DataFrame):
+        assert hasattr(dataframe, "gst")
+        assert isinstance(dataframe.gst, GeostFrame)
+
+        with pytest.raises(
+            KeyError,
+            match="DataFrame must contain an 'nr' column identifying individual objects.",
+        ):
+            df_invalid = pd.DataFrame({"x": [1, 2], "y": [3, 4]})
+            df_invalid.gst  # Should raise an error because 'nr' column is missing
+
+    @pytest.mark.unittest
+    def test_has_geometry(
+        self, dataframe: pd.DataFrame, geodataframe: gpd.GeoDataFrame
+    ):
+        assert not dataframe.gst.has_geometry
+        assert geodataframe.gst.has_geometry
+
+    @pytest.mark.unittest
+    def test_check_has_spatial(
+        self, dataframe: pd.DataFrame, geodataframe: gpd.GeoDataFrame
+    ):
+        with pytest.raises(
+            TypeError,
+            match="Object is not a GeoDataFrame with a valid geometry column.",
+        ):
+            dataframe.gst._check_has_spatial()
+        geodataframe.gst._check_has_spatial()  # Should not raise an error for geodataframe
+
+    @pytest.mark.unittest
+    def test_to_iterable(self, borehole_data):
+        inst = "string"
+        inst = borehole_data.gst._to_iterable(inst)
+        assert isinstance(inst, list)
+
+        inst = ["list of strings"]
+        inst = borehole_data.gst._to_iterable(inst)
+        assert isinstance(inst, list)
+
+    @pytest.mark.unittest
+    def test_select_within_bbox(self, point_header):
+        selected = point_header.gst.select_within_bbox(1, 1, 3, 3)
+        assert len(selected) == 9
+
+        # Test inverted selection: everything outside the bbox
+        selected = point_header.gst.select_within_bbox(1, 1, 3, 3, invert=True)
+        assert len(selected) == 16
+
+        with pytest.raises(TypeError):
+            point_header = point_header.drop(columns="geometry")
+            # Remove geometry column to make it invalid for spatial selection
+            point_header.gst.select_within_bbox(1, 1, 3, 3)
+
+    @pytest.mark.unittest
+    def test_select_with_points(self, point_header):
+        selection_points = gpd.GeoDataFrame(
+            geometry=gpd.points_from_xy([1, 4, 1, 4], [1, 4, 4, 1]), crs=28992
+        )
+        max_distance = 1.1
+        selected = point_header.gst.select_with_points(selection_points, max_distance)
+        assert len(selected) == 16
+
+        inverted_selection = point_header.gst.select_with_points(
+            selection_points, max_distance, invert=True
+        )
+        assert len(inverted_selection) == 9
+
+        # Select with Shapely object
+        selected = point_header.gst.select_with_points(
+            selection_points["geometry"].iloc[0], max_distance
+        )
+        assert len(selected) == 3
+
+        # Select with iterable
+        selected = point_header.gst.select_with_points(
+            selection_points["geometry"].to_list(), max_distance
+        )
+        assert len(selected) == 16
+
+        with pytest.raises(TypeError):
+            point_header = point_header.drop(columns="geometry")
+            # Remove geometry column to make it invalid for spatial selection
+            point_header.gst.select_with_points(selection_points, max_distance)
+
+    @pytest.mark.unittest
+    def test_select_with_lines(self, point_header):
+        selection_lines = [LineString([[1, 1], [5, 5]]), LineString([[1, 5], [5, 1]])]
+        selection_gdf = gpd.GeoDataFrame(geometry=selection_lines, crs=28992)
+
+        max_distance = 1
+        selected = point_header.gst.select_with_lines(selection_gdf, max_distance)
+        assert len(selected) == 21
+
+        inverted_selection = point_header.gst.select_with_lines(
+            selection_gdf, max_distance, invert=True
+        )
+        assert len(inverted_selection) == 4
+
+        # Select with Shapely object
+        selected = point_header.gst.select_with_lines(selection_lines[0], max_distance)
+        assert len(selected) == 13
+
+        # Select with iterable
+        selected = point_header.gst.select_with_lines(selection_lines, max_distance)
+        assert len(selected) == 21
+
+        with pytest.raises(TypeError):
+            point_header = point_header.drop(columns="geometry")
+            # Remove geometry column to make it invalid for spatial selection
+            point_header.gst.select_with_lines(selection_gdf, max_distance)
+
+    @pytest.mark.unittest
+    def test_select_within_polygons(self, point_header):
+        selection_polygon = Polygon(((2, 1), (5, 4), (4, 5), (1, 2)))
+        selection_gdf = gpd.GeoDataFrame(geometry=[selection_polygon], crs=28992)
+
+        # Geodataframe based selection
+        selected = point_header.gst.select_within_polygons(selection_gdf)
+        assert len(selected) == 3
+
+        selected_inverted = point_header.gst.select_within_polygons(
+            selection_gdf, invert=True
+        )
+        assert len(selected_inverted) == 22
+
+        selected_buffered = point_header.gst.select_within_polygons(
+            selection_gdf, buffer=0.1
+        )
+        assert len(selected_buffered) == 11
+
+        selected_inverted_buffered = point_header.gst.select_within_polygons(
+            selection_gdf, buffer=0.1, invert=True
+        )
+        assert len(selected_inverted_buffered) == 14
+
+        # Shapely Polygon based selection
+        selected = point_header.gst.select_within_polygons(selection_polygon)
+        assert len(selected) == 3
+        assert_array_equal(selected["nr"], ["nr7", "nr13", "nr19"])
+
+        # Selection with Iterable
+        selected = point_header.gst.select_within_polygons([selection_polygon])
+        assert len(selected) == 3
+
+        with pytest.raises(TypeError):
+            point_header = point_header.drop(columns="geometry")
+            # Remove geometry column to make it invalid for spatial selection
+            point_header.gst.select_within_polygons(selection_gdf)
+
+    @pytest.mark.unittest
+    def test_select_by_values(self, borehole_data):
+        selected = borehole_data.gst.select_by_values("lith", ["V", "K"], how="or")
+        assert isinstance(selected, pd.DataFrame)
+
+        expected_nrs = ["A", "B", "C", "D"]
+        selected_nrs = selected["nr"].unique()
+        assert_array_equal(selected_nrs, expected_nrs)
+
+        selected = borehole_data.gst.select_by_values("lith", ["V", "K"], how="and")
+
+        expected_nrs = ["B", "D"]
+        selected_nrs = selected["nr"].unique()
+
+        assert_array_equal(selected_nrs, expected_nrs)
+
+    @pytest.mark.unittest
+    def test_slice_by_values(self, borehole_data):
+        sliced = borehole_data.gst.slice_by_values("lith", "Z")
+        assert isinstance(sliced, pd.DataFrame)
+
+        expected_boreholes_with_sand = ["A", "C", "D", "E"]
+        expected_length = 10
+
+        assert_array_equal(sliced["nr"].unique(), expected_boreholes_with_sand)
+        assert (sliced["lith"] == "Z").all()
+        assert len(sliced) == expected_length
+
+        sliced = borehole_data.gst.slice_by_values("lith", "Z", invert=True)
+
+        expected_boreholes_without_sand = ["A", "B", "C", "D"]
+        expected_length = 15
+
+        assert_array_equal(sliced["nr"].unique(), expected_boreholes_without_sand)
+        assert not (sliced["lith"] == "Z").any()
+        assert len(sliced) == expected_length
+
+    @pytest.mark.unittest
+    def test_select_by_condition(self, borehole_data):
+        selected = borehole_data.gst.select_by_condition(borehole_data["lith"] == "V")
+        assert isinstance(selected, pd.DataFrame)
+
+        expected_nrs = ["B", "D"]
+        assert_array_equal(selected["nr"].unique(), expected_nrs)
+        assert (selected["lith"] == "V").all()
+        assert len(selected) == 4
+
+        selected = borehole_data.gst.select_by_condition(
+            borehole_data["lith"] == "V", invert=True
+        )
+        assert len(selected) == 21
+        assert not (selected["lith"] == "V").all()
 
 
 class TestHeaderAccessor:
